d0532704b9ceeb949bc3251e6f763efea9fb1fdd
Nguyen Minh Hien <nguyenminhhien@gmail.com>
2019-08-02 15:11:27 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img v-show="showLogo" @click="showLogo=!showLogo"  alt="Vue logo" src="./assets/logo.png">
+    
+    <HelloWorld v-for="x in 10" :msg="title + x" :key="x"/>
+    
+    <ul>
+      <li v-for="(value, name, index) in list" :key="value">
+        {{index+1}}. {{name.charAt(0).toUpperCase()+name.substring(1)}} : {{value}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { setTimeout } from 'timers';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data:function() {
+    return{
+      showLogo:true,
+      title: 'Hi there',
+      list: {
+        name:'Hien',
+        age: 38
+      }          
+    }
+  },
+  watch:{
+    showLogo:function(newVal){
+      if(!newVal){
+        setTimeout(() => this.showLogo=true,1000)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
